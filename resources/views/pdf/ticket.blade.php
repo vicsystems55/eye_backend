794429dd1a37b7f6e54191b5bd05f708b0ffe232
vicSystems <victorasuquob@gmail.com>
2022-09-26 15:55:08 +0100

    icon change

--- resources/views/pdf/ticket.blade.php
+++ resources/views/pdf/ticket.blade.php
@@ -21,6 +21,6 @@
 <body style="background-image: url(data:image/png;base64,{{ base64_encode(file_get_contents(public_path('cert_background2.jpg'))) }});
 background-size: contain;
 ">
-    <h1 style="text-align: center; margin-top: 300px;" class="cali">Victor Asuquo</h1>
+    <h1 style="text-align: center; margin-top: 300px;" >Victor Asuquo</h1>
 </body>
 </html>
