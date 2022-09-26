f99acca0aeb3dfe2a4bb4ae6e42a740088b06148
vicSystems <victorasuquob@gmail.com>
2022-09-26 15:40:33 +0100

    icon change

--- resources/views/pdf/ticket.blade.php
+++ resources/views/pdf/ticket.blade.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
     <title>Document</title>
 
     <style>
         @page { margin: 0px; }
         body { margin: 0px; }
+
+        .cali{
+            font-family: 'Kaushan Script', cursive;
+        }
     </style>
 </head>
 <body style="background-image: url(data:image/png;base64,{{ base64_encode(file_get_contents(public_path('cert_background2.jpg'))) }});
 background-size: contain;
 ">
-    <h1 style="text-align: center; margin-top: 300px;">Victor Asuquo</h1>
+    <h1 style="text-align: center; margin-top: 300px;" class="cali">Victor Asuquo</h1>
 </body>
 </html>
