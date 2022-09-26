666a23692191a33d33e86caf37398948184ef568
vicSystems <victorasuquob@gmail.com>
2022-09-26 15:31:12 +0100

    icon change

--- resources/views/pdf/ticket.blade.php
+++ resources/views/pdf/ticket.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <style>
+        @page { margin: 0px; }
+        body { margin: 0px; }
+    </style>
 </head>
 <body style="background-image: url(data:image/png;base64,{{ base64_encode(file_get_contents(public_path('cert_background2.jpg'))) }});
 background-size: contain;
